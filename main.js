5f87821396082bb2d0f37e610db307d588f95efa
TheOruntoEniola <31780542+TheOruntoEniola@users.noreply.github.com>
2022-01-06 15:36:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ window.onload=function() {
         bod.style.opacity= "0.2";
         contact.style.top = "100px";
         exchange.style.top = "179px";
+        menu.removeEventListener("click", glamour)
         menu.addEventListener("click", normal);
     }
 
@@ -24,6 +25,32 @@ window.onload=function() {
         contact.style.top = "-100px";
         exchange.style.top = "-100px";
         bod.style.opacity= "1";
+        menu.removeEventListener("click", normal)
         menu.addEventListener("click", glamour);
+    }
+
+
+    //Scroller code
+    var to = document.getElementById("to");
+    var fro = document.getElementById("fro");
+    var screen1 = document.getElementById("screen1");
+    var screen2 = document.getElementById("screen2");
+    var screen3 = document.getElementById("screen3");
+    var screen4 = document.getElementById("screen4");
+    var phone1 = document.getElementById("phone1");
+    var phone2 = document.getElementById("phone2");
+    var phone3 = document.getElementById("phone3");
+    var phone4 = document.getElementById("phone4");
+
+
+    to.addEventListener("click", slideto);
+    fro.addEventListener("click", slidefrom);
+
+    function slideto () {
+        screen1.style.left= "";
+    }
+
+    function slidefrom () {
+
     }
 }
